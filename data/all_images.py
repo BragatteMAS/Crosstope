1819529a358646b6ef69bc390b1be7a36e940391
Bragatte <marcelobragatte@gmail.com>
2020-11-12 17:18:37 -0300

    updates

---
+++ data/all_images.py
@@ -0,0 +1,23 @@
+#images.py
+#Ref.:https://www.youtube.com/watch?v=MjwWzBiAMck
+
+import requests, os, bs4
+from requests import get
+
+domain = "http://www.crosstope.com"
+#define separated html page dir
+page = requests.get("http://www.crosstope.com/uploads/Imagens_GRASP/V5/")
+#list all elements in the page select
+html = page.text
+soup = BeautifulSoup(html, "html.parser")
+
+#function get every link in the page as element "a" in href
+for link in soup.find_all('a'):
+    url = link.get('href')
+    #in this line change for the extension needed
+    if ".jpg" in url:
+        print(url)
+        file_name = url.split('V5/',1)[1]
+        with open(file_name, "wb") as file:
+            response = get(domain + url)
+            file.write(response.content)
