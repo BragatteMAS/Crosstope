94c411b5fb8cdaa55d98ef0489411a4c3f95df9a
Bragatte <marcelobragatte@gmail.com>
2021-04-10 19:28:43 -0300

    sPYpdate

--- analyses/img_proc/spyder.py
+++ analyses/img_proc/spyder.py
@@ -4,15 +4,26 @@
 Created on Sat Dec 26 18:06:27 2020
 
 @author: bragatte
+
+#working directory
+##Win10: C:\Users\marce\Documents\GitHub\Crosstope
+##PopOS: ~/Documents/GitHub/Crosstope
+
+scikit-image: pip install scikit-image
+opencv: pip install opencv-python
 """
+pip install skimage
+
 #scikit-learn
 from skimage import io #read RGB
-img1 = io.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/A0201_0001_V5.jpg')
-imgt1= io.imread('/home/bragatte/Documentos/GitHub/html-css/Estudos/html-css/ex000/cross200.png')
+img1 = io.imread('C:\Users\marce\Documents\GitHub\Crosstope\data\images\grasp\A0201_0001_V5.jpg') #win
+#img1 = io.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/A0201_0001_V5.jpg')
+#imgt1= io.imread('/home/bragatte/Documentos/GitHub/html-css/Estudos/html-css/ex000/cross200.png')
 
 import cv2 #read BRG
-img2 = cv2.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/A0201_0001_V5.jpg')
-imgt2= cv2.imread('/home/bragatte/Documentos/GitHub/html-css/Estudos/html-css/ex000/cross200.png')
+img2 = cv2.imread('C:\Users\marce\Documents\GitHub\Crosstope\data\images\grasp\A0201_0001_V5.jpg')
+#img2 = cv2.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/A0201_0001_V5.jpg')
+#imgt2= cv2.imread('/home/bragatte/Documentos/GitHub/html-css/Estudos/html-css/ex000/cross200.png')
 
 import numpy as np
 a=np.ones((5,5))
