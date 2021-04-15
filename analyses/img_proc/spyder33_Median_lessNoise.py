96ae059388ef628d984f5a424cec02f3bfc18c64
Bragatte <marcelobragatte@gmail.com>
2021-04-14 21:16:39 -0300

    Update spyder33_Median_lessNoise.py

--- analyses/img_proc/spyder33_Median_lessNoise.py
+++ analyses/img_proc/spyder33_Median_lessNoise.py
@@ -14,23 +14,23 @@ from skimage.filters import median
 
 
 #Needs 8 bit, not float.
-img_gaussian_noise = cv2.imread('/data/images/grasp/zikv/ALPVYLMTL_5K_noise.png', 0)
-img_salt_pepper_noise = cv2.imread('/data/images/grasp/zikv/ALPVYLMTL_5K_noise.png', 0)
+img_gaussian_noise = cv2.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/zikv/ALPVYLMTL_5K_noise.png', 0)
+img_salt_pepper_noise = cv2.imread('/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/zikv/ALPVYLMTL_5K_noise.png', 0)
 
 img = img_salt_pepper_noise
 
-
-median_using_cv2 = cv2.medianBlur(img, 3)
+median_using_cv2 = cv2.medianBlur(img, 3) #3 kernel size
+###
+disc_example =  disk(3)
+disc_example
+###
 
 from skimage.morphology import disk  
 #Disk creates a circular structuring element, similar to a mask with specific radius
 median_using_skimage = median(img, disk(3), mode='constant', cval=0.0)
 
-
 cv2.imshow("Original", img)
 cv2.imshow("cv2 median", median_using_cv2)
 cv2.imshow("Using skimage median", median_using_skimage)
-
 cv2.waitKey(0)          
 cv2.destroyAllWindows() 
-
