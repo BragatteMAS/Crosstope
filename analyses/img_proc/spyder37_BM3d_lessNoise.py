46663deae8a356e2ad2c7061b368a7411342338e
Bragatte <marcelobragatte@gmail.com>
2021-04-25 12:49:04 -0300

    Best denoising bm3

--- analyses/img_proc/spyder37_BM3d_lessNoise.py
+++ analyses/img_proc/spyder37_BM3d_lessNoise.py
@@ -7,16 +7,18 @@ Created on Thu Apr 15 21:59:53 2021
 
 http://www.cs.tut.fi/~foi/papers/ICIP2019_Ymir.pdf
 """
-
 from skimage import io, img_as_float
 import bm3d
 import cv2
 
-noisy_img = img_as_float(io.imread("images/Osteosarcoma_01_25Sigma_noise.tif", as_gray=True))
+noisy_img = img_as_float(io.imread("/home/bragatte/Documentos/GitHub/Crosstope/data/images/grasp/zikv/ALPVYLMTL_5K_noise.png", as_gray=False))
 
-BM3D_denoised_image = bm3d.bm3d(noisy_img, sigma_psd=0.2, stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
+BM3D_denoised_image = bm3d.bm3d(noisy_img, sigma_psd=0.05, stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
 
 """
+Best Denoising pack:
+    Could use in color img
+    0.5 sigma great results
 bm3d library is not well documented yet, but looking into source code....
 sigma_psd - noise standard deviation
 stage_arg: Determines whether to perform hard-thresholding or Wiener filtering.
@@ -28,5 +30,3 @@ cv2.imshow("Original", noisy_img)
 cv2.imshow("Denoised", BM3D_denoised_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
