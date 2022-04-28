566c7b492dec2d9e8db40d9f94736108bf6f1776
Bragatte <marcelobragatte@gmail.com>
2022-04-28 11:02:20 -0300

    jquery

---
+++ public/js/jquery.js
@@ -0,0 +1,13 @@
+/* target="_blank" rel="noreferrer, nofollow, noopener, external" */
+
+function externalLinks() {
+    if (!document.getElementsByTagName) return;
+    var anchors = document.getElementsByTagName("a");
+    for (var i=0; i<anchors.length; i++) {
+    var anchor = anchors[i];
+    if (anchor.getAttribute("href") &&
+    anchor.getAttribute("rel") == "external")
+    anchor.target = "_blank";
+    }
+    }
+    window.onload = externalLinks;
